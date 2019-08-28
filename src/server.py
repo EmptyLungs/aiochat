1920a7c8a0d0a29845f7c1c07d9a939b2252cdec
empty <dmitryrint@gmail.com>
2019-08-28 14:41:08 +0700

    back to high-level app and server

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 import aioredis
 import peewee_async
@@ -11,45 +12,26 @@ from db import database
 from utils.middlewares import request_user_middleware
 from settings import REDIS_CON, DATABASE
 
-from views import Test
+from views import Test, WebSocket, LogInView
 
-async def start_app(pool):
-    router = UrlDispatcher()
-    router.add_get('/', Test)
-    loop = asyncio.get_event_loop()
-    redis_pool = await aioredis.create_pool(REDIS_CON, loop=loop)
-    middlewares = [session_middleware(RedisStorage(redis_pool=redis_pool, cookie_name='aiochat_session_id')), request_user_middleware]
-    app = web.Application(router=router, middlewares=middlewares)
-    app.redis_pool = redis_pool
+router = UrlDispatcher()
+router.add_get('/', Test)
+router.add_get('/ws', WebSocket)
+router.add_post('/login', LogInView)
 
-    database.init(**DATABASE)
-    app.database = database
-    app.database.set_allow_sync(False)
-    app.objects = peewee_async.Manager(app.database)
-    # web.run_app(app, host='0.0.0.0', port=8001)
-    server_handler = app.make_handler()
-    server_gen = loop.create_server(server_handler, '0.0.0.0', 8001)
-    return server_gen, server_handler, app
+loop = asyncio.get_event_loop()
+redis_pool = loop.run_until_complete(aioredis.create_pool(REDIS_CON, loop=loop))
+middlewares = [session_middleware(RedisStorage(redis_pool=redis_pool, cookie_name='aiochat_session_id', max_age=500)), 
+               request_user_middleware]
+app = web.Application(router=router, middlewares=middlewares)
+app.redis_pool = redis_pool
+app.ws_connections = {}
 
-async def stop_app(server, app, handler):
-    server.close()
-    await server.wait_closed()
-    app.redis_pool.close()
-    await app.redis_pool.wait_closed()
-    await app.objects.close()
-    await app.shutdown()
-    await app.cleanup()
+database.init(**DATABASE)
+app.database = database
+app.database.set_allow_sync(False)
+app.objects = peewee_async.Manager(app.database)
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    server_gen, handler, app = loop.run_until_complete(start_app(loop))
-    server = loop.run_until_complete(server_gen)
+logging.basicConfig(level=logging.DEBUG)
 
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        print('Keyboard interrupt')
-    finally:
-        loop.run_until_complete(stop_app(server, app, handler))
-        loop.close()
-    print('Server killed')
+web.run_app(app, host='0.0.0.0', port=8001)
