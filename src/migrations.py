6451df7dfde8a528a2baad2781134d8699048230
empty <dmitryrint@gmail.com>
2019-08-30 01:17:46 +0700

    chat room migration

--- src/migrations.py
+++ src/migrations.py
@@ -2,7 +2,7 @@ import asyncio
 
 import peewee_async
 
-from models import User, Message
+from models import User, Message, ChatRoom
 from db import database
 from settings import DATABASE
 
@@ -12,6 +12,13 @@ manager = peewee_async.Manager(database)
 with manager.allow_sync():
     User.create_table(True)
     Message.create_table(True)
+    ChatRoom.create_table(True)
     
     for username in ['User1', 'User2', 'User3']:
-        User.create(username=username, password='123')
+        try:
+            User.create(username=username, password='123')
+        except:
+            pass
+    default_user = User.get(User.id == 1)
+    for room in ['flood', 'nsfw', 'music']:
+        ChatRoom.create(name=room, owner=default_user)
