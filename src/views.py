179daf8a932136a33f57aece99c368eb9c500382
empty <dmitryrint@gmail.com>
2019-08-30 01:19:51 +0700

    new apis, some ws struggles

--- src/views.py
+++ src/views.py
@@ -1,8 +1,9 @@
 from time import time
+import uuid
 
 from aiohttp import web, WSMsgType
 
-from models import Message, User
+from models import Message, User, ChatRoom
 
 
 class BaseView(web.View):
@@ -21,23 +22,47 @@ class Test(BaseView):
 class WebSocket(BaseView):
     login_required = True
     async def get(self):
+        app = self.request.app
+        user = self.request.user
+        try:
+            self.room = await app.objects.get(ChatRoom, id=self.request.match_info['slug'].lower())
+        except ChatRoom.DoesNotExist:
+            return web.HTTPNotFound()
         ws = web.WebSocketResponse()
         await ws.prepare(self.request)
+
+        if self.room.id not in app.ws_connections:
+            app.ws_connections[self.room.id] = {}
+        
+        message = await app.objects.create(
+            Message, room=self.room, user=None, text="@{} joined chat!".format(user.username if user else 'anon')
+        )
+        app.ws_connections[self.room.id][user.id if user else uuid.uuid4()] = ws
+        await self.broadcast(message)
+
         async for msg in ws:
             if msg.type == WSMsgType.TEXT:
                 if msg.data == 'close':
                     await ws.close()
                 else:
-                    await ws.send_str(msg.data + '/answer')
+                    text = msg.data.strip()
+                    message = await app.objects.create(Message, room=self.room, user=user, text=text)
+                    await self.broadcast(message)
             elif (msg.type == WSMsgType.ERROR):
-                self.request.app.logger.dubg('ws connection error: {}'.format(ws.exception()))
+                app.logger.dubg('ws connection error: {}'.format(ws.exception()))
         return ws
 
+    async def broadcast(self, message):
+        for conn in self.request.app.ws_connections[self.room.id].values():
+            print(conn)
+            await conn.send_json(message.as_dict())
+            # await conn.send_str(message.text)
 
-class LogInView(web.View):
+class LogInView(BaseView):
     async def post(self):
         data = await self.request.json()
         app = self.request.app
+        print(data)
         username = data.get('username')
         password = data.get('password')
         if not username or not password:
@@ -45,7 +70,21 @@ class LogInView(web.View):
         try:
             user = await app.objects.get(User, username=username, password=password)
         except User.DoesNotExist:
-            return web.HTTPBadRequest('Wrong login or password, try again!')
+            return web.HTTPBadRequest()
         self.request.session['user'] = str(user.id)
         self.request.session['time'] = time()
-        return web.HTTPOk()
+        return web.json_response(user.as_dict())
+
+class UserInfoView(BaseView):
+    login_required = True
+    async def get(self):
+        return web.json_response(self.request.user.as_dict())
+
+
+class RoomListView(BaseView):
+    login_required = True
+    async def get(self):
+        # ohuet' mojno
+        rooms = await self.request.app.objects.execute(ChatRoom.select(ChatRoom, User).join(User))
+        resp = list(map(ChatRoom.as_dict, rooms))
+        return web.json_response(resp)
