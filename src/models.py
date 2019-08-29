c6040a6f923f6d6bee2ef400d6af4756fe3c2145
empty <dmitryrint@gmail.com>
2019-08-30 01:17:02 +0700

    refactor models, add as_dict model methods

--- src/models.py
+++ src/models.py
@@ -8,7 +8,40 @@ class User(BaseModel):
     # TODO: passlib.hash - sha256_crypt.verify(password, hash)
     password = CharField(max_length=20)
 
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username
+        }
+
+
+class ChatRoom(BaseModel):
+    name = CharField(max_length=20, unique=True)
+    owner = ForeignKeyField(rel_model=User, related_name='owner', on_delete='SET NULL')
+
+    @classmethod
+    async def all_messages(cls, objects):
+        return await objects.prefetch(self.messages, User.select())
+
+    def as_dict(self):
+        print(self)
+        return {
+            "id": self.id,
+            "name": self.name,
+            "owner": self.owner.as_dict()
+        }
+
 
 class Message(BaseModel):
     text = TextField()
-    user = ForeignKeyField(rel_model=User, related_name='messages', on_delete='CASCADE')
+    user = ForeignKeyField(rel_model=User, related_name='messages', on_delete='CASCADE', null=True)
+    room = ForeignKeyField(rel_model=ChatRoom, related_name='messages', on_delete='CASCADE')
+
+    class Meta:
+        order_by = ('created',)
+
+    def as_dict(self):
+        return {
+            "text": self.text, "created": self.created.isoformat(),
+            "user": {"username": self.user.username, "id": self.user.id} if self.user else None
+        }
